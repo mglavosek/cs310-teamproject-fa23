e6eca7d500c8e4e4d31c5489517e3bbad2a2b59a
Spencer Carden <scarden1@stu.jsu.edu>
2023-10-30 12:08:28 -0500

    Seeing if adjust works or not :D

--- src/edu/jsu/mcis/cs310/tas_fa23/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/Punch.java
@@ -1,12 +1,21 @@
 
 package edu.jsu.mcis.cs310.tas_fa23;
+import static edu.jsu.mcis.cs310.tas_fa23.EventType.*;
+import static edu.jsu.mcis.cs310.tas_fa23.PunchAdjustmentType.*;
 import edu.jsu.mcis.cs310.tas_fa23.Shift;
+import edu.jsu.mcis.cs310.tas_fa23.dao.BadgeDAO;
 import edu.jsu.mcis.cs310.tas_fa23.dao.DAOFactory;
+import edu.jsu.mcis.cs310.tas_fa23.dao.EmployeeDAO;    
+import edu.jsu.mcis.cs310.tas_fa23.dao.PunchDAO;
 import edu.jsu.mcis.cs310.tas_fa23.dao.ShiftDAO;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit.*;
 import java.time.format.DateTimeFormatter;
 import java.time.DayOfWeek;
-import java.time.temporal.ChronoUnit;
+import java.time.LocalTime;
+import static java.time.temporal.ChronoUnit.MINUTES;
+
+
 
 public class Punch {
     private final Integer terminalid;
@@ -14,8 +23,8 @@ public class Punch {
     private final EventType punchType;
     private final Badge badge;
     private LocalDateTime originalTimeStamp = null;
-    private final LocalDateTime adjustedTimeStamp = null;
-    private PunchAdjustmentType adjustmentType = NONE;
+    private LocalDateTime adjustedTimeStamp = null;
+    private PunchAdjustmentType adjustmentType = null;
     
     
  public Punch(Integer terminalid, Badge badge, EventType punchType) {
@@ -34,38 +43,105 @@ public class Punch {
         this.punchType = punchType;      
 }
   public void adjust(Shift s){
-        //connecting to shift dao
-        DAOFactory daoFactory = new DAOFactory("tas.jdbc");
-        ShiftDAO ShiftDao = daoFactory.getShiftDAO();
-        
-        //get shift DAO
-        
-        //s=ShiftDAO.find(Something that traverses and reads all the shifts);
-        
-        
-        
         LocalDateTime ots = originalTimeStamp;
-        LocalTime shiftstart = s.getShiftStart();
-        LocalTime shiftstop = s.getShiftStop();
-        LocalTime lunchstart = s.getLunchStart();
-        LocalTime lunchstop = s.getLunchStop();
+        LocalDateTime shiftstart = originalTimeStamp.toLocalDate().atTime(s.getShiftStart());       
+        LocalDateTime shiftstop = originalTimeStamp.toLocalDate().atTime(s.getShiftStop());
+        LocalDateTime lunchstart = originalTimeStamp.toLocalDate().atTime(s.getLunchStart());
+        LocalDateTime lunchstop = originalTimeStamp.toLocalDate().atTime(s.getLunchStop());
         int roundInterval = s.getRoundInterval();
         int gracePeriod = s.getGracePeriod();
         int dockPenalty = s.getDockPenalty();
         int minutesOver = ots.getMinute() % roundInterval;
-        
+           
         ////checks
         
-        //compare clock in, check grace period
-        if(this.getPunchType() == CLOCK_IN){
+        
+        
+        
+        //turn everything into one unit using the minutes between and duration methods
+        //format it back into the correct kind for the junittest.
+        
+     
+        if(this.getPunchType() == CLOCK_IN){    
             
-            if (shiftGrace){
-                
+            //Within Grace Period
+            if (MINUTES.between(originalTimeStamp,shiftstart)< gracePeriod && originalTimeStamp.isAfter(shiftstart)){
+                //round down to shiftstart 
+                adjustedTimeStamp=shiftstart;
+                adjustmentType = PunchAdjustmentType.SHIFT_START;                                                                                                                      }
+            
+            //intervalround up to shiftstart 
+            else if (MINUTES.between(originalTimeStamp,shiftstart) < roundInterval && originalTimeStamp.isBefore(shiftstart)){
+                // round up to shiftstart 
+                adjustedTimeStamp=shiftstart;
+                adjustmentType = PunchAdjustmentType.INTERVAL_ROUND;    
             }
-        }
+            
+            //Clock in Dock Penalty for clocking in after graceperiod
+            else if (MINUTES.between(originalTimeStamp,shiftstart)> gracePeriod && originalTimeStamp.isAfter(shiftstart)){
+            
+                adjustedTimeStamp=shiftstart;
+                adjustmentType = PunchAdjustmentType.SHIFT_START;                                                                               
+            }
+            
+            //no Adjustments needed as it is within an increment set by the roundInterval
+            else if ((MINUTES.between (originalTimeStamp,shiftstart)%roundInterval == 0) && (originalTimeStamp.isAfter(shiftstart))){                      originalTimeStamp=shiftstart;
+                     adjustmentType = PunchAdjustmentType.SHIFT_START;
+            }
+            //no Adjustments needed as it is a correct clock in
+            else if (originalTimeStamp==shiftstart){
+                    originalTimeStamp=shiftstart;
+                    
+            }
+            else if ((adjustmentType==PunchAdjustmentType.LUNCH_START) && MINUTES.between(originalTimeStamp,lunchstart)< roundInterval){
+                
+                
+            }        
+       
+        } 
+ 
+                
+        else if (this.getPunchType()== CLOCK_OUT){
+         if (MINUTES.between(originalTimeStamp,shiftstop)< gracePeriod && originalTimeStamp.isBefore(shiftstop)){
+                //round up to shiftstop
+                adjustedTimeStamp=shiftstop;
+                adjustmentType = PunchAdjustmentType.SHIFT_STOP;                                                                                                                      }
+            
+
+            else if (MINUTES.between(originalTimeStamp,shiftstop) < roundInterval && originalTimeStamp.isAfter(shiftstop)){
+                //interval round down to shiftstop
+                adjustedTimeStamp=shiftstop;
+                adjustmentType = PunchAdjustmentType.INTERVAL_ROUND;    
+            }
+            
+           
+            else if (MINUTES.between(originalTimeStamp,shiftstop)> gracePeriod && originalTimeStamp.isBefore(shiftstop)){
+                //Clock in Dock Penalty for clocking in after graceperiod
+                adjustedTimeStamp=shiftstart;
+                adjustmentType = PunchAdjustmentType.SHIFT_STOP;                                                                               
+            }
+            
+            
+            else if ((MINUTES.between (originalTimeStamp,shiftstop)%roundInterval == 0) && (originalTimeStamp.isBefore(shiftstop))){                    //no Adjustments needed as it is within an increment set by the roundInterval
+                originalTimeStamp=shiftstop;
+                adjustmentType = PunchAdjustmentType.SHIFT_STOP;
+           
+            }           
+            else if (originalTimeStamp==shiftstop){
+                //no Adjustments needed as it is a correct clock in
+                originalTimeStamp=shiftstop;
+            }   
+            
+        }    
         
+  
+  
+       
+            
+            
+            
         
-      
+  
   }
   
   public int getId() {
